6a5c32a5f0c77cfffb023628959de7e4d3507adb
JenB <ntdat.c302@gmail.com>
2025-02-11 18:11:23 +0700

    Update Lab1_Bai2
    Nhập chiều dài và rộng, tính chu vi, diện tích, tìm cạnh nhỏ nhất.

--- Lab1/src/Lab1_Bai2.java
+++ Lab1/src/Lab1_Bai2.java
@@ -2,6 +2,8 @@
  * 
  */
 
+import java.util.Scanner;
+
 /**
  * 
  */
@@ -12,7 +14,26 @@ public class Lab1_Bai2 {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		Scanner nhap = new Scanner(System.in);
+		
+		System.out.print("Nhập chiều dài: ");
+		double dai = nhap.nextDouble();
+		
+		System.out.print("Nhập chiều rộng: ");
+		double rong = nhap.nextDouble();
+		
+		double chuVi = (dai+rong)*2;
+		
+		double dienTich = dai * rong;
+		
+		double canhNhoNhat = Math.min(dai, rong);
+		
+		System.out.printf("Chu vi = %.1f",  chuVi);
+		
+		System.out.printf("\nDiện tích = %.1f",  dienTich);
+		
+		System.out.printf("\nCạnh nhỏ nhất = %.1f",  canhNhoNhat);
 	}
 
 }
